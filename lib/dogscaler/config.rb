ffaa74965cc8b5372b3d8d053d88f4ef51b2d335
Gibbons, David <dgibbons@crowdcompass.com>
2017-05-02 09:32:02 -0700

    Updating the configuration documentation

--- lib/dogscaler/config.rb
+++ lib/dogscaler/config.rb
@@ -4,40 +4,42 @@ module Dogscaler
       config = <<-END
 # Enter your datadog api credentials here:
 datadog:
-  api_key:
-  application_key:
-# This assumes you have a .aws/credentials file, if you use multiple, you can specify which profile to use here.
+  api_key: KEYHERE
+  application_key: KEYHERE
+# This assumes you have a ~/.aws/credentials file, if you use multiple, you can specify which profile to use here.
 aws:
-  region: 'us-west-2'
-  profile: 'default'
+  region: us-west-2
+  profile: default
+slack:
+  channel: '#slack'
+  api_token: TOKENHERE
 
 # This is a list of events to check for. Each event will be checked on each run.
 instances:
-  -
-    name: 'instance1'
-    query: "example datadog query: eg max:system.load.1{env:production}"
-    autoscale_group: "Name of an autoscale group to scale"
-    scale_up_threshhold: 75   # scale up if the query surpasses this values
-    scale_down_threshhold: 20 # scale down if the query goes below this value
-    min_instances: 3          # Minimum number of systems to have in the autoscale group
-    max_instances: 5          # Maximum number of systems to have in the autoscale group
-    grow_by: 2                # How many instances to add to the autoscale group
-    shrink_by: 1              # how many instances to remove from the autoscale group
-    transform: min            # What transform to use on the query, eg (min,max,last,avg,count)
-  -
-    name: 'instance2'
-    query: "example datadog query: eg max:system.load.1{env:production}"
-    autoscale_tag_filters:      # Alternatively use a set of tags to find your instance
-      - Name: nginx             # name is key,  "nginx" is the tag Value
-      - Environment: Production # Environment is the key, production is the value
-    scale_up_threshhold: 100    # scale up if the query surpasses this values
-    scale_down_threshhold: 40   # scale down if the query goes below this value
-    min_instances: 3            # Minimum number of systems to have in the autoscale group
-    max_instances: 5            # Maximum number of systems to have in the autoscale group
-    grow_by: 2                  # How many instances to add to the autoscale group
-    shrink_by: 1                # how many instances to remove from the autoscale group
-    transform: avg              # What transform to use on the query, eg (min,max,last,avg,count)
-
+  'core':
+    queries:
+      - name: 'scale on cpu user'
+        query: avg:system.cpu.user{env:production,type:core}
+        scale_up_threshhold: 10    # scale up if the query surpasses this values
+        scale_down_threshhold: 5   # scale down if the query goes below this value
+        transform: avg             # What transform to use on the query, eg (min,max,last,avg,count)
+    shrink_by: 1                   # How many instances to add to the autoscale group
+    grow_by: 1                     # how many instances to remove from the autoscale group
+    asg_tag_filters:               # key value tags of filter and find our autoscale group.
+      Type: core
+      Environment: production
+  'mailer':
+    queries:
+      - name: 'scale on user invites'
+        query: max:mailer.db.v3.unsent_invitations{*}
+        scale_up_threshhold: 1
+        scale_down_threshhold: 0
+        transform: avg
+    grow_by: 1
+    shrink_by: 1
+    asg_tag_filters:
+      Type: mailer
+      Environment: production
 END
         puts config
       end
