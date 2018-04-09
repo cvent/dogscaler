5579833bc1d746be8251a013ba443e3974e00f15
Gibbons, David <dgibbons@crowdcompass.com>
2018-04-09 10:19:42 -0700

    Bugfiles:
    fix state file handling, our config was always defaulting to {}.
    
    fix scaling to max, add conditionals for when we will scale over max but
    arent yet to max

--- lib/dogscaler/state.rb
+++ lib/dogscaler/state.rb
@@ -5,21 +5,25 @@ module Dogscaler
   	  @state = self.load
       @config = '/tmp/dogscaler.yaml'
   	end
+
   	def load
       begin
-    	  YAML.load_file(config)
+    	  YAML.load_file(@config)
       rescue
         {}
       end
   	end
+
   	def get(asg)
   	  @state[asg] || Time.parse("2017-01-31 12:00:00")
   	end
+
   	def update(asg)
       t = Time.now
       logger.debug "Updating the timestamp for #{asg} to #{t}"
   	  @state[asg] = t
   	end
+
     def save!
       logger.debug "Saving state to #{@config}"
       File.open( @config, 'w' ) do |out|
